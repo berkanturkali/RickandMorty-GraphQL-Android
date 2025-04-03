bcba079d2f01895a014fbeb55b4244e6f4abab21
Berkan Türkali <berkanturkali@outlook.com>
2025-04-03 19:42:44 +0300

    setup graphql by adding Apollo Runtime client and create Characters query

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.apollo.runtime)
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.example")
+    }
 }
 
 android {
@@ -56,4 +63,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //apollo
+    implementation(libs.apollo.runtime)
 }
